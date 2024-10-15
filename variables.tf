99d4d9c2ef52c3f2474b4316d6344e6ae15e645f
zhengb8 <zhengb8@chinatelecom.cn>
2024-10-15 16:39:27 +0800

    change test

--- variables.tf
+++ variables.tf
@@ -1,4 +1,9 @@
 variable "provider_token" {
   type = string
-  default = "l550MWYd0oACZA.atlasv1.pf10vaPNDWbSMHl9q8PkCeRDaWnLRcVSvdqtv1qAwYk1opyHctrMovnrm8h6xDTQko0"
+  default = ""
+}
+
+variable "test" {
+  type = string
+  default = "test"
 }
